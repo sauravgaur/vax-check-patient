e1a28d954aa1f31a6e4f4096736072642bdf185a
Siddharth Patel <siddhu.grr8@gmail.com>
2021-06-15 11:38:43 +0530

    update prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	isMockEnabled: false, // You have to switch this, when your real back-end is done
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
-	local_url: 'https://vaxcheck.firstvitals.com/traveler',
+	local_url: 'https://vaxcheck.firstvitals.com/corporate',
 	stripe_key: 'pk_test_51IllDFIoULcjF60KfMk1Jbb3COmYyAbZ1QLxLRMugIk3WU6p2k2nWp5YgCPTswBQqhbLjQspbDeONmOmh7z8r1hv00LOfVmm6o',
-	api_url: 'http://api-vaxcheck.firstvitals.com/api'
+	api_url: 'https://api-vaxcheck.firstvitals.com/api'
 };
